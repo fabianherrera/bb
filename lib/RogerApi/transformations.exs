48165d736f41464270731b7ea149594a2a5de8e9
Fabian Herrera <fabianherrera@gmail.com>
2018-02-09 21:41:31 -0400

    feat: create transformate func

--- lib/RogerApi/transformations.exs
+++ lib/RogerApi/transformations.exs
@@ -26,19 +26,39 @@ defmodule RogerApi.Transformations do
 
 
   """
+## Activity 1: Incomplete / Closed
+#  def transformate([]), do: []
+#  def transformate([x]), do: [transformate_node_tree(x)]
+#  def transformate([head | tail]), do: [transformate_node_tree(head) | transformate(tail)]
+#
+#  defp transformate_node_tree([]), do: []
+#  defp transformate_node_tree({_node, node_data}), do: [transformate_status_tree([node_data])]
+#
+#  defp transformate_status_tree([]), do: []
+#  defp transformate_status_tree([x]), do: [transformate_partition_tree(x)]
+#  defp transformate_status_tree([head1 | tail1]), do: [transformate_partition_tree(head) | transformate_status_tree(tail)]
+
+# Activity 2: Incomplete / in Process
 
   def transformate([]), do: []
-  def transformate([x]), do: [transformate_node_tree(x)]
-  def transformate([head | tail]), do: [transformate_node_tree(head) | transformate(tail)]
+  def transformate(x)  do
+                        Keyword.values(x)
+                        |> Enum.filter(fn(x) -> x != %{} end)
+                        |> [transformate_root_tree()]
 
-  defp transformate_node_tree([]), do: []
-  defp transformate_node_tree({_node, node_data}), do: [transformate_status_tree([node_data])]
+  end
 
-  defp transformate_status_tree([]), do: []
-  defp transformate_status_tree([x]), do: [transformate_partition_tree(x)]
-  defp transformate_status_tree([head1 | tail1]), do: [transformate_partition_tree(head) | transformate_status_tree(tail)]
+  # def transformate([head | tail]), do: [transformate_node_tree(head) | transformate(tail)]
 
+  defp transformate_root_tree([]), do: []
+  defp transformate_root_tree([head | tail]), do: [transformate_partition_tree(head) | transformate_root_tree(tail)]
 
+  defp transformate_partition_tree([]), do: []
+  defp transformate_partition_tree(x) do
+                Map.to_list(x)
+                |> Keyword.values()
+                |> List.flatten()
+end
 
 end
 
