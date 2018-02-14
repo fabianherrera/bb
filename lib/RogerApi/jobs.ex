8198a6054514c76190f901a1ff41f6c763aa5804
Fabian Herrera <fabianherrera@gmail.com>
2018-02-14 19:18:14 -0400

    docs: update jobs docs

--- lib/RogerApi/jobs.ex
+++ lib/RogerApi/jobs.ex
@@ -1,4 +1,38 @@
 defmodule RogerApi.Jobs do
+  @moduledoc """
+  Documentation for Jobs.
+  """
+
+  @doc """
+  Given a nested data structure, where each element contents nested items:
+  [
+      "watcher@127.0.0.1": %{
+        "roger_demo_partition" => [
+          %Roger.Job{
+            args: %{"country" => "Venezuela", "email" => "janedoe@gmail.com", "name" => "Jane Doe", "number_of_pets" => 966},
+            execution_key: nil, id: "16ovjr39jvijf4kgrlqe2ib2aadojhid", module: RogerDemo.Job.CreateUpdateUser,
+            queue_key: nil, queued_at: 1517697586453, retry_count: 0, started_at: 1517697682999
+          }
+  ]]
+
+  return a sorted Map with the following structure:
+  [
+  %Roger.Job{
+    args: %{
+      "country" => "Venezuela",
+      "email" => "janedoe@gmail.com",
+      "name" => "Jane Doe",
+      "number_of_pets" => 967
+    },
+    execution_key: nil,
+    id: "c2mkvrgfonkgf1pdgkdl87okb3hi103r",
+    module: RogerDemo.Job.CreateUpdateUser,
+    queue_key: nil,
+    queued_at: 1517697586454,
+    retry_count: 0,
+    started_at: 1517697683000
+  }]
+  """
 
   def running_jobs([]), do: []
   def running_jobs(jobs), do:  jobs |> Keyword.values() |> Enum.flat_map(&Map.values/1) |> List.flatten()
--- lib/RogerApi/queues.ex
+++ lib/RogerApi/queues.ex
@@ -1,4 +1,49 @@
 defmodule RogerApi.Queues do
-  def running_partitions([]), do: []
-  def running_partitions(jobs), do:  jobs |> Keyword.values() |> Enum.flat_map(&Map.values/1) |> List.flatten()
+  @moduledoc """
+  Documentation for Queues.
+  """
+
+  @doc """
+  Given a nested data structure, where each element contents nested items:
+    input = [
+      "server@127.0.0.1": %{
+        running: %{
+          "roger_test_partition_1" => %{
+            default: %{consumer_count: 1, max_workers: 10, message_count: 740, paused: false},
+            fast: %{consumer_count: 1, max_workers: 10, message_count: 740, paused: false},
+            other: %{consumer_count: 1, max_workers: 2, message_count: 0, paused: false}
+          }]
+
+  return a sorted Map with the following keys:
+  [
+  %{
+    "count" => 740,
+    "partition_name" => "roger_test_partition_2",
+    "paused" => "running",
+    "qualified_queue_name" => "roger_test_partition_2-default",
+    "queue_name" => :default
+  }]
+
+
+  """
+
+
+  def partitions([]), do: []
+  def partitions(partitions) do
+    partitions
+    |> Keyword.values()
+    |> Enum.flat_map(&(Enum.flat_map(&1, consolidate)))
+  end
+
+  # Incomplete
+  defp consolidate{k, v} do
+  #  capture_partition_key(v) ->
+    
+  end
+
+
 end
+
+
+
+
